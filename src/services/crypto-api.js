a97403b7bcd59890a907a7b7bed5fa6333ffe734
subhradwip01 <subhradwip01@gmail.com>
2022-02-10 13:13:25 +0530

    Basic Api setup and show basic data in home page
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ src/services/crypto-api.js
@@ -0,0 +1,58 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+
+const cryptoApiHeaders ={
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '372c6143afmsha772f70b592105fp15bfd2jsn7b791276b84b'
+}
+
+const baseUrl="https://coinranking1.p.rapidapi.com"
+
+const createRequest = (url) => ({
+    url,
+    headers:cryptoApiHeaders
+})
+
+
+export const cryptoApi = createApi({
+    reducerPath: "cryptoApi",
+    baseQuery : fetchBaseQuery({baseUrl}),
+    endpoints:(builder)=>({
+        getCryptos: builder.query({
+            query: ()=>createRequest("/coins")
+
+        })
+
+    })
+})
+
+export const {
+    useGetCryptosQuery
+} = cryptoApi
+
+
+// var axios = require("axios").default;
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     tiers: '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': '372c6143afmsha772f70b592105fp15bfd2jsn7b791276b84b'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
