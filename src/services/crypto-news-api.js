b4082268b6a66d37bb0857a113fc69db275e5de1
subhradwip01 <subhradwip01@gmail.com>
2022-02-11 11:52:35 +0530

    Crypto news api created
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ src/services/crypto-news-api.js
@@ -0,0 +1,29 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+
+const cryptoNewsApiHeaders ={
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '372c6143afmsha772f70b592105fp15bfd2jsn7b791276b84b'
+}
+
+const baseUrl ="https://bing-news-search1.p.rapidapi.com"
+
+const createRequest = (url) => ({
+    url,
+    headers:cryptoNewsApiHeaders
+})
+
+const cryptoNewsApi=createApi({
+    reducerPath: "cryptoNewsApi",
+    baseQuery:  fetchBaseQuery({baseUrl}),
+    endpoints:(builder)=>({
+        getCryptoNews: builder.query({
+            query: ({newsCatgory,count})=>createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+
+        })
+
+    })
+})
+
+
+export const {useCryptoNewsQuery} = cryptoNewsApi
