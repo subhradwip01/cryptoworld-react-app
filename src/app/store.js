e1c6cadc51d96b95fe24736b5e09d887ebe5744b
subhradwip01 <subhradwip01@gmail.com>
2022-02-11 20:53:32 +0530

    connecting news api to redux store
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { cryptoApi } from "../services/crypto-api";
+import { cryptoApi} from "../services/crypto-api";
+import { cryptoNewsApi } from "../services/crypto-news-api";
 
 
 
 export default configureStore({
     reducer:{
-        [cryptoApi.reducerPath]:cryptoApi.reducer
+        [cryptoApi.reducerPath]:cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]:cryptoNewsApi.reducer
     }
 })
