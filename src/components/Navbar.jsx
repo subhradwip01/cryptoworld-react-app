b6621b422a7cf412f10f76860f8f7ca9ec5a48f2
subhradwip01 <subhradwip01@gmail.com>
2022-02-12 19:06:07 +0530

    Mobile navbar fixed
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import { Button,Menu,Typography,Avatar } from "antd";
 import {Link} from "react-router-dom"
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined } from "@ant-design/icons/lib/icons";
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined,MenuOutlined } from "@ant-design/icons/lib/icons";
 
 import icon from "../images/cryptocurrency.png"
 
 const Navbar = () => {
+    const [activeMenu,setActiveMenu]=useState(true)
+    const [screenSize,setScreenSize]=useState(null)
+
+    useEffect(() => {
+      const handleResize=()=>setScreenSize(window.innerWidth)
+
+      window.addEventListener("resize",handleResize);
+
+      handleResize();
+
+      return ()=>window.removeEventListener("resize".handleResize)
+    }, [])
+    
+
+    useEffect(() => {
+      if(screenSize<768){
+          setActiveMenu(false)
+      }else{
+          setActiveMenu(true)
+      }
+    }, [screenSize])
+    
+
   return (
     <div className="nav-container">
         <div className="logo-container">
@@ -13,7 +36,11 @@ const Navbar = () => {
             <Typography.Title level={2} className="logo">
                 <Link to="/">CryptoWorld</Link>
             </Typography.Title>
+            <Button className="menu-control-conatainer" onClick={()=>setActiveMenu(true)}>
+                <MenuOutlined/>
+            </Button>
         </div>
+        {activeMenu && 
             <Menu theme="dark">
                 <Menu.Item icon={<HomeOutlined/>}>
                     <Link to="/">Home</Link>
@@ -28,6 +55,7 @@ const Navbar = () => {
                     <Link to="/news">News</Link>
                 </Menu.Item>
             </Menu>
+            }
         
     </div>
   )
