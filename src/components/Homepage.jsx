d53fa4af384f5881c64e19c813458f80ba7a4ec2
Subhradwip Kulavi <88609894+subhradwip01@users.noreply.github.com>
2024-01-24 00:57:33 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -34,10 +34,6 @@ const Homepage = () => {
         <Title level={3} className='show-more'><Link to="/cryptocurrencies">Show more</Link></Title>
       </div>
       <Cryptocurrencies simplified/>
-      <div className='home-heading-container'>
-        <Title level={2} className='home-title'>Latest Crypto News</Title>
-        <Title level={3} className='show-more'><Link to="/cryptocurrencies">Show more</Link></Title>
-      </div>
     </div>
   )
 }
